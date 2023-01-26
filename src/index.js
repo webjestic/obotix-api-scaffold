53f503f7df3010a8b601f61479a345e0289ff0b7
Michael N <michael.newlon@tn.gov>
2023-01-26 06:08:17 -0500

    obotix update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 
+
 import obotix from 'obotix'
-
 await obotix.init()
-const log = obotix.logger.getLogger('app:index')
-obotix.logger.setLogLevel('trace')
 
-obotix.listen( () => {
-    log.info(`Service Listening on ${process.env.OBOTIX_PORT}`)
+
+const app = obotix.getApp()
+const log = obotix.getLogger('main:index')
+
+app.use(obotix.addUrlEncodedMiddleware())
+app.use(obotix.addHealthzRouter())
+
+// add routers here
+
+app.use(obotix.addErrorHandlingMiddleware())
+
+
+const port = process.env.OAPI_PORT || 3000
+app.listen(port, () => {
+    log.info(`HTTP Server is listening on port ${port}. PID:${process.pid}`)
 })
+
+
