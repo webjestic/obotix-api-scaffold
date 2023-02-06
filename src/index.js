22cbd6aaa63682ba17815fa47dd498c1c4a25dac
Michael N <michael.newlon@tn.gov>
2023-02-05 20:37:40 -0500

    i29 | fix: fixed issue with app.listen not loading mw

--- src/index.js
+++ src/index.js
@@ -8,15 +8,16 @@ import fs from 'fs'
 await obotix.init()
 
 const log = obotix.getLogger('src:index')
-const app = obotix.getApp()
+
 
 // Add middleware & Routes here
 // ex:
+// const app = obotix.getApp()
 // app.use('/', myRoute(obotix.getRouter()) )
 
 
 const port = process.env.OAPI_PORT || 3000
-app.listen(port, () => {
+obotix.listen(port, () => {
     obotix.sys.displayResources(log)
     let pckg = JSON.parse(fs.readFileSync('package.json', 'utf8'))
     log.info(`${pckg.name} ${pckg.version} is listening on port ${port}. PID: ${process.pid}`)
